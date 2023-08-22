d6fb0b60b56ae9930b9943f03a0ffdd1546c236b
rajnandini190 <142484988+rajnandini190@users.noreply.github.com>
2023-08-23 03:47:39 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function myFunction(){
-    const middle = document.getElementById("main");
-    let maindiv = document.getElementById("m")
+    const middle = document.getElementById("main_section");
+    let maindiv = document.getElementById("folderStructure")
       if (!middle || !mainDiv) {
         console.error("One or both of the elements were not found.");
         return;
