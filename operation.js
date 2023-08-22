c7a08156bdbb7d268c10a7674fe25972bf1973eb
rajnandini190 <142484988+rajnandini190@users.noreply.github.com>
2023-08-23 03:52:19 +0530

    operation.js

--- operation.js
+++ operation.js
@@ -61,7 +61,7 @@ const onClickFolder = (event) => {
 
     iterateChildrenAndPush(parentItem.id, data, newItem);
     
-    let mainDiv = document.getElementById("main");
+    let mainDiv = document.getElementById("folderStructure");
     mainDiv.innerHTML = '';
     parentid = 0;
 
@@ -77,7 +77,7 @@ const onClickFile = (event) => {
     iterateChildrenAndPush(parentItem.id, data, newItem);
 
    
-    let mainDiv = document.getElementById("main");
+    let mainDiv = document.getElementById("folderStructure");
     mainDiv.innerHTML = '';
     parentid = 0;
     iterateChildrenForPrint(mainDiv, data[0].children);  
@@ -86,50 +86,50 @@ const onClickFile = (event) => {
 const onClickDelete = (event) =>{
      let parentId = event.parentNode.parentNode.id;
     deleteNode(parentId, data, event.parentNode.id);
-    let mainDiv = document.getElementById("main");
+    let mainDiv = document.getElementById("folderStructure");
     mainDiv.innerHTML = '';
     parentid = 0;
     iterateChildrenForPrint(mainDiv, data[0].children);
 }
 
-const renameItem = (folderList, itemId) => {
-    for (let item of folderList) {
-        if (item.id == itemId) {
-            let newName = prompt("Please enter a new name for the file/folder.");
-            let element = document.getElementById(itemId);
-            let parentItem = iterateDataForGettingParent(store, element.parentNode.id);
-            for(const child of parentItem.children){
-                if(child.name === newName){
-                    alert("A folder/file with this name already exists in this folder. Please choose a different name.");
-                    return;
-                }
-            }
-            console.log(element);
-            let childCount = element.childElementCount;
-            if(newName != null && newName != ""){
-                console.log(checkFileName(newName));
-                while(!checkFileName(newName)){
-                    window.alert("The name should be less than 10 characters and should not contain numbers.");
-                    newName = window.prompt("Please try again!");
-                }
-                item.name = newName;
-                console.log(childCount);
-                let requiredElement;
-                if(childCount === 3){
-                    requiredElement = element.firstElementChild;
-                }else{
-                    requiredElement = element.firstElementChild.nextElementSibling;
-                }
-                console.log(requiredElement);
-                requiredElement.innerHTML = newName;
-            }
-            checkFileName("");
-        }
-        if (item.type == "folder") {
-            renameItem(item.children, itemId);
-        }
-    }
-}
+// const renameItem = (folderList, itemId) => {
+//     for (let item of folderList) {
+//         if (item.id == itemId) {
+//             let newName = prompt("Please enter a new name for the file/folder.");
+//             let element = document.getElementById(itemId);
+//             let parentItem = iterateDataForGettingParent(store, element.parentNode.id);
+//             for(const child of parentItem.children){
+//                 if(child.name === newName){
+//                     alert("A folder/file with this name already exists in this folder. Please choose a different name.");
+//                     return;
+//                 }
+//             }
+//             console.log(element);
+//             let childCount = element.childElementCount;
+//             if(newName != null && newName != ""){
+//                 console.log(checkFileName(newName));
+//                 while(!checkFileName(newName)){
+//                     window.alert("The name should be less than 10 characters and should not contain numbers.");
+//                     newName = window.prompt("Please try again!");
+//                 }
+//                 item.name = newName;
+//                 console.log(childCount);
+//                 let requiredElement;
+//                 if(childCount === 3){
+//                     requiredElement = element.firstElementChild;
+//                 }else{
+//                     requiredElement = element.firstElementChild.nextElementSibling;
+//                 }
+//                 console.log(requiredElement);
+//                 requiredElement.innerHTML = newName;
+//             }
+//             checkFileName("");
+//         }
+//         if (item.type == "folder") {
+//             renameItem(item.children, itemId);
+//         }
+//     }
+// }
 
 
 
